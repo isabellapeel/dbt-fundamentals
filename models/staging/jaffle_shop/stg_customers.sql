33d26591a9393872fecd94fb724cb28618a4b8bd
Isabella Peel <isabella.peel@aginic.com>
2022-01-14 05:28:31 +0000

    jaffle_shop_project

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,8 @@
+-- create stage customers seperate model 
+
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from raw.jaffle_shop.customers
---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,10 @@
+
+---- create stage. orders seperate model 
+
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from raw.jaffle_shop.orders
